5db4e6ac3ef6996a9d249e2bfb2400e48873543f
PedroAntunesCosta <47991446+PedroAntunesCosta@users.noreply.github.com>
2024-11-07 12:54:10 -0300

    Update troubleshooting-structure-adjustment.js

--- docs-utils/troubleshooting-structure-adjustment.js
+++ docs-utils/troubleshooting-structure-adjustment.js
@@ -3,10 +3,32 @@ const path = require('path');
 
 const locales = ['en', 'pt', 'es']
 
+function moveContent() {
 for (locale of locales) {
-    const oldPath = path.resolve(`../help-center-content/docs/${locale}/tutorials/troubleshooting`)
-    const newPath = path.resolve(`../help-center-content/docs/${locale}/troubleshooting`)
-    console.log(oldPath)
-    console.log(newPath)
-    fs.renameSync(oldPath, newPath)
+      const oldPath = path.resolve(`../help-center-content/docs/${locale}/tutorials/troubleshooting`)
+      const newPath = path.resolve(`../help-center-content/docs/${locale}/troubleshooting`)
+      fs.renameSync(oldPath, newPath)
+  }
 }
+
+function moveArticleTagsToFrontmatter() {
+
+    const testingPath = path.resolve(`../help-center-content/docs/en/troubleshooting/Integrations/integration-errors-in-amazon-orders.md`)
+
+    const fileContent = fs.readFileSync(testingPath, 'utf-8')
+
+    const tags = fileContent.split('**Tags:**')[1].split('\n')[0]
+    const frontMatter = fileContent.split('---')[1]
+    const text = fileContent.split('---')[2].replace(/^.*Tags:.*$/m, '').trim()
+
+    // Assemble everything
+    const newContent = '---'+frontMatter+`tags: ${tags}\n---\n\n`+text
+
+    console.log(newContent)
+}
+
+function moveAllTags() {}
+
+// moveContent()
+// moveAllTags()
+moveArticleTagsToFrontmatter()
