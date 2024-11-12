fa8dead2a2b05796c70f8c6ca4853f20753db285
PedroAntunesCosta <47991446+PedroAntunesCosta@users.noreply.github.com>
2024-11-12 13:04:44 -0300

    Update troubleshooting-structure-adjustment.js

--- docs-utils/troubleshooting-structure-adjustment.js
+++ docs-utils/troubleshooting-structure-adjustment.js
@@ -36,7 +36,7 @@ function moveAllTags(folderPath) {
 }
 
 async function adjustTroubleshootingContent() {
-    console.log('Adjusting troubleshooting content')
+    console.log('Adjusting troubleshooting content...')
     for (locale of locales) {
         // const oldPath = path.join(__dirname,`../../docs/${locale}/tutorials/troubleshooting`)
         const newPath = path.join(__dirname,`../../docs/${locale}/troubleshooting`)
