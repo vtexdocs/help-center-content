25609d71d9a2d993d1671458bd667b3de209f716
PedroAntunesCosta <47991446+PedroAntunesCosta@users.noreply.github.com>
2024-11-12 13:26:19 -0300

    Update troubleshooting-structure-adjustment.js

--- docs-utils/troubleshooting-structure-adjustment.js
+++ docs-utils/troubleshooting-structure-adjustment.js
@@ -39,8 +39,8 @@ function moveAllTags(folderPath) {
 async function adjustTroubleshootingContent() {
     console.log('Adjusting troubleshooting content...')
     for (locale of locales) {
-        const oldPath = path.join(__dirname,`../../docs/${locale}/tutorials/troubleshooting`)
-        const newPath = path.join(__dirname,`../../docs/${locale}/troubleshooting`)
+        const oldPath = path.join(`/docs/${locale}/tutorials/troubleshooting`)
+        const newPath = path.join(`/docs/${locale}/troubleshooting`)
         fs.renameSync(oldPath, newPath)
         moveAllTags(newPath)
     }
