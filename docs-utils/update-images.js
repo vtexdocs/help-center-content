4bb8ba176b4769e3f22c2426f0e9f7c7115d5825
julia-rabello <77292838+julia-rabello@users.noreply.github.com>
2024-11-19 16:13:18 -0300

    edit update-images.js

--- docs-utils/update-images.js
+++ docs-utils/update-images.js
@@ -1,78 +1,87 @@
-const fs = require('fs')
-const path = require('path')
-// const frontmatter = require('front-matter')
-const imageDownloader = require('image-downloader')
+const fs = require('fs');
+const path = require('path');
+const imageDownloader = require('image-downloader');
 
-const baseURL = 'https://raw.githubusercontent.com/vtexdocs/help-center-content/refs/heads/main/'
+const baseURL = 'https://raw.githubusercontent.com/vtexdocs/help-center-content/refs/heads/main/';
 
-const isValidExtension = (ext) => {
-  return /^[a-zA-Z0-9]*$/.test(ext)
-}
+const isValidExtension = (ext) => /^[a-zA-Z0-9]*$/.test(ext);
 
 const getExtension = (url) => {
-  let dotIndex = url.length - 1
-  while (dotIndex >= 0 && url[dotIndex] !== '.') {
-    dotIndex--
-  }
-
-  const ext = url.substring(dotIndex + 1)
-  if (dotIndex < 0 || !isValidExtension(ext)) return 'png'
-  return ext
-}
+  let dotIndex = url.lastIndexOf('.');
+  if (dotIndex < 0) return 'png'; // Default to 'png' if no extension
+  const ext = url.substring(dotIndex + 1);
+  return isValidExtension(ext) ? ext : 'png';
+};
 
 const updateImages = async (filepath) => {
+  console.log('Running update-images.js');
 
-  console.log ('Running update-images.js')
   if (!filepath) {
     console.error('Received undefined filePath in updateImages');
-    return; // Early return if filePath is undefined
+    return;
   }
-  const content = fs.readFileSync(filepath, 'utf-8')
-  
-  const images = []
-  let imageIndex = 1
+
+  const resolvedFilepath = path.resolve(filepath); // Ensure the path is absolute
+  console.log(`Processing file: ${resolvedFilepath}`);
+
+  if (!fs.existsSync(resolvedFilepath)) {
+    console.error(`File not found: ${resolvedFilepath}`);
+    return;
+  }
+
+  const content = fs.readFileSync(resolvedFilepath, 'utf-8');
+
+  const images = [];
+  let imageIndex = 1;
+
   const replaceURL = (match, extra, url) => {
-    const isMarkdownBlock = match.startsWith('![')
-    if (url.startsWith(baseURL)) return match
+    const isMarkdownBlock = match.startsWith('![');
+    if (url.startsWith(baseURL)) return match;
 
     if (url.startsWith('//')) {
-      url = `https:${url}`
+      url = `https:${url}`;
     }
 
-    let newURL = ''
-    const ext = getExtension(url)
-    
-    const newfilepath = `${filepath.split('.')[0]}_${imageIndex}.${ext}`
+    const ext = getExtension(url);
+    const parsedPath = path.parse(resolvedFilepath); // Safely parse the path
+    const newFilepath = parsedPath.dir + '/' + `${parsedPath.name}_${imageIndex}.${ext}`; // Create new file path
+
     images.push({
-      filepath: path.resolve(newfilepath),
-      url
-    })
-    
-    newURL = `${baseURL}${filepath.split('.')[0]}_${imageIndex}.${ext}`
-    imageIndex++
-    
-    return isMarkdownBlock ? `![${extra}](${newURL})` : `<img ${extra}src="${newURL}"`
-  }
+      filepath: path.resolve(newFilepath),
+      url,
+    });
+''
+    const encodedFilepath = encodeURI(path.relative('.', newFilepath).replace(/\\/g, "/"))
+
+    console.log('ENCODEDFILEPATH:' + encodedFilepath)
+
+
+    const newURL = `${baseURL}${encodedFilepath}`;
+    imageIndex++;
+
+    return isMarkdownBlock ? `![${extra}](${newURL})` : `<img ${extra}src="${newURL}"`;
+  };
 
   const newContent = content
     .replace(/\!\[(.*)\]\((.*)\)/g, replaceURL)
-    .replace(/<img (.*)src="(.*)"/g, replaceURL)
+    .replace(/<img (.*)src="(.*)"/g, replaceURL);
 
   try {
-    for (let i = 0; i < images.length; i++) {
+    for (const image of images) {
       await imageDownloader.image({
-        url: images[i].url,
-        dest: images[i].filepath
-      })
+        url: image.url,
+        dest: image.filepath,
+      });
     }
-  
-    fs.writeFileSync(filepath, newContent)
-  } catch (err) {
-    console.log("Couldn't download some of the images, leaving file as it is...\n\t",filepath)
-  }
-}
 
-const filepath = process.argv[2]
+    fs.writeFileSync(resolvedFilepath, newContent);
+    console.log(`Updated file: ${resolvedFilepath}`);
+  } catch (err) {
+    console.error(`Error updating images for: ${resolvedFilepath}`, err);
+  }
+};
+
+const filepath = process.argv[2] // run with this command: node docs-utils/update-images.js "{fullFilePath}"
 updateImages(filepath)
 
 module.exports = { updateImages };
